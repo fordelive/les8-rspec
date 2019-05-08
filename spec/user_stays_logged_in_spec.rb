c46dd5fae0b51edfc5d60fddee3eda39f4dc904d
Volodymyr Shkriabynets <vshkriabynets@strongqa.com>
2019-05-08 12:52:54 +0300

    User stays logged out TC 03.3

--- spec/user_stays_logged_in_spec.rb
+++ spec/user_stays_logged_in_spec.rb
@@ -25,4 +25,31 @@ RSpec.feature 'Remember Me' do
       HomePage.on { expect(main_menu_section).to be_login_successful }
     end
   end
+
+
+  context 'when User is logged in with remember-me feature and logs out' do
+    it 'User stays logged out when Home page is reopened' do
+      login_hash = { login: resolve_login(:correct), password: resolve_password(:correct),
+                     remember: true }
+      LoginPage.open
+      LoginPage.on { log_user_in(login_hash) }
+
+      HomePage.on { expect(main_menu_section).to be_login_successful }
+
+      HomePage.on { main_menu_section.logout }
+
+      session_cookies = Capybara.page.driver.browser.manage.all_cookies
+
+      Capybara.reset_sessions!
+
+      HomePage.open
+      HomePage.on {driver.browser.manage.delete_all_cookies}
+
+      session_cookies.each { |cookie| HomePage.on { driver.browser.manage.add_cookie(cookie) }}
+
+      HomePage.on { reload }
+
+      HomePage.on { expect(main_menu_section).to be_logout_successful }
+    end
+  end
 end
